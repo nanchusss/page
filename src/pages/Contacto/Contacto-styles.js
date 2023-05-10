5bf11e57565fc15790e249f13cdb6bf09df19149
nanchusss <nancyart83@gmail.com>
2023-05-10 23:30:14 +0200

    dd

--- src/pages/Contacto/Contacto-styles.js
+++ src/pages/Contacto/Contacto-styles.js
@@ -3,16 +3,16 @@ import { Button, Form } from "react-bootstrap";
 
 export const StyledFormContainer = styled.div`
   top: 60%;
+  margin-top: 150px;
   margin-left: auto;
   margin-right: auto;
-
+  width: 50%;
   background-color: #ffffff;
   box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.2);
   padding: 20px;
 
   @media (max-width: 768px) {
     width: 90%;
-
     margin-top: 140px;
   }
 `;
